49c3ea315cad7ff0289729895ea2b7f365bc5079
boatengine <thanapat.boat16@gmail.com>
2026-01-09 00:50:34 +0700

    merge report to dev

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/eiei",
+    element: <div>Hello World</div>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Theme>
+      <RouterProvider router={router} />,
+    </Theme>
+  </StrictMode>
+);
