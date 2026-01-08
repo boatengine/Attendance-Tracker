720d57bce326bc07684057d7a4b4dacbe053f68e
boatengine <thanapat.boat16@gmail.com>
2026-01-09 00:54:40 +0700

    update route

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,17 @@
 import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+
 import authRoute from "./routes/auth.js";
 import attendanceRoute from "./routes/attendance.js";
 import employeeRoute from "./routes/employee.js";
+import locationRoute from "./routes/location.js";
+import reportRoute from "./routes/report.js";
+
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +19,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoute);
 app.use("/api/attendance", attendanceRoute);
 app.use("/api/employee", employeeRoute);
+app.use("/api/location", locationRoute);
+app.use("/api/report", reportRoute);
+
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
