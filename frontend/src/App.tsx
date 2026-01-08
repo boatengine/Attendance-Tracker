981c75a3721fb645550f151727912ade1a87e045
boatengine <thanapat.boat16@gmail.com>
2026-01-09 04:07:40 +0700

    restructure code frontend jsx -> tsx and install base and use shadcn

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: 
+  },
+]);
+
+export default function App() {
+  return (
+    <>
+      <RouterProvider router={router} />,
+    </>
+  );
+}
