55cdbc4c90058607299d88984e73924e1a8b5ce0
boatengine <thanapat.boat16@gmail.com>
2026-01-09 10:31:00 +0700

    bypass cors with creden

--- backend/app.js
+++ backend/app.js
@@ -12,11 +12,16 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use("/api/auth", authRoute);
 app.use("/api/attendance", attendanceRoute);
