c44702dd1878043df13075fcdb77b578f593bd92
Boatengine <61369952+boatengine@users.noreply.github.com>
2026-01-09 18:42:50 +0700

    Update attendance_tracker.sql

--- attendance_tracker.sql
+++ attendance_tracker.sql
@@ -3,7 +3,7 @@
 -- Host: localhost    Database: attendance_tracker
 -- ------------------------------------------------------
 -- Server version	8.0.42
-create database attendance_tracker
+create database attendance_tracker;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
