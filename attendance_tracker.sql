076333752eb31f8ca8aeac7f2a64b476197dc744
boatengine <thanapat.boat16@gmail.com>
2026-01-28 12:33:59 +0700

    add vercel

--- attendance_tracker.sql
+++ attendance_tracker.sql
@@ -1,29 +1,35 @@
 create database attendance_tracker;
+use attendance_tracker;
+CREATE TABLE `employees` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `employee_id` varchar(20) NOT NULL,
+  `pin` varchar(6) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `department` varchar(100) DEFAULT NULL,
+  `position` varchar(100) DEFAULT NULL,
+  `face_encoding` text,
+  `auth_location_id` int DEFAULT NULL,
+  `is_active` tinyint(1) DEFAULT '1',
+  `is_admin` tinyint(1) DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `employee_id` (`employee_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- MySQL dump 10.13  Distrib 8.0.43, for macos15 (arm64)
---
--- Host: localhost    Database: attendance_tracker
--- ------------------------------------------------------
--- Server version	8.0.42
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE TABLE `authorized_locations` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `latitude` decimal(10,8) NOT NULL,
+  `longitude` decimal(11,8) NOT NULL,
+  `radius_meters` int DEFAULT '200',
+  `is_active` tinyint(1) DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Table structure for table `attendance_records`
---
-
-DROP TABLE IF EXISTS `attendance_records`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `attendance_records` (
   `id` int NOT NULL AUTO_INCREMENT,
   `auth_location_id` int NOT NULL,
@@ -46,89 +52,7 @@ CREATE TABLE `attendance_records` (
   CONSTRAINT `fk_attendance_employee` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_attendance_location` FOREIGN KEY (`auth_location_id`) REFERENCES `authorized_locations` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `attendance_records`
---
 
-LOCK TABLES `attendance_records` WRITE;
-/*!40000 ALTER TABLE `attendance_records` DISABLE KEYS */;
-/*!40000 ALTER TABLE `attendance_records` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `authorized_locations`
---
-
-DROP TABLE IF EXISTS `authorized_locations`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `authorized_locations` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `latitude` decimal(10,8) NOT NULL,
-  `longitude` decimal(11,8) NOT NULL,
-  `radius_meters` int DEFAULT '200',
-  `is_active` tinyint(1) DEFAULT '1',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `authorized_locations`
---
-
-LOCK TABLES `authorized_locations` WRITE;
-/*!40000 ALTER TABLE `authorized_locations` DISABLE KEYS */;
-INSERT INTO `authorized_locations` VALUES (3,'sirinthorn',13.78384420,100.49336050,200,1,'2026-01-09 05:34:00'),(4,'office 1 mahidol salaya',13.80928210,100.32096950,200,1,'2026-01-09 05:34:41');
-/*!40000 ALTER TABLE `authorized_locations` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `employees`
---
-
-DROP TABLE IF EXISTS `employees`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `employees` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `employee_id` varchar(20) NOT NULL,
-  `pin` varchar(6) NOT NULL,
-  `full_name` varchar(255) NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `department` varchar(100) DEFAULT NULL,
-  `position` varchar(100) DEFAULT NULL,
-  `face_encoding` text,
-  `auth_location_id` int DEFAULT NULL,
-  `is_active` tinyint(1) DEFAULT '1',
-  `is_admin` tinyint(1) DEFAULT '0',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `employee_id` (`employee_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `employees`
---
-
-LOCK TABLES `employees` WRITE;
-/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
 INSERT INTO `employees` VALUES (8,'ADMIN001','123456','admin','admin@gmail.com',NULL,NULL,NULL,NULL,1,1,'2026-01-09 11:18:38','2026-01-09 11:18:52'),(9,'EMP001','123456','nalanta phuket','nalantaphuket@gmail.com','HR',NULL,NULL,4,1,0,'2026-01-28 04:21:46','2026-01-28 04:21:46');
-/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2026-01-28 11:52:44
+INSERT INTO `authorized_locations` VALUES (3,'sirinthorn',13.78384420,100.49336050,200,1,'2026-01-09 05:34:00'),(4,'office 1 mahidol salaya',13.80928210,100.32096950,200,1,'2026-01-09 05:34:41');
