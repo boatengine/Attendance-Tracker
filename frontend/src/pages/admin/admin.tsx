7dcde71857e642e933979433177d0e01d34c3563
boatengine <thanapat.boat16@gmail.com>
2026-01-28 12:06:05 +0700

    edit somthing

--- frontend/src/pages/admin/admin.tsx
+++ frontend/src/pages/admin/admin.tsx
@@ -14,6 +14,7 @@ import {
   XCircle,
   LogOut,
 } from "lucide-react";
+// fix
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
